762a9b1977f47017c88f94f1bd73ea74ff4b7f84
Idriss Gackou <rgackou@hotmail.co.uk>
2018-05-26 11:14:18 -0400

    Create Test.r

---
+++ Test.r
@@ -0,0 +1,21 @@
+height <- c(59,60,61,58,67,72,70)
+weight <- c(150,140,180,220,160,140,130)
+
+a <- 150
+
+
+# calculate average height 
+mean(height)
+# calculate average weight
+mean(weight)
+
+# calculate length of vector height, weight
+lenHeight <- length(height)
+lenWeight <- length(weight)
+
+# Sum the heights
+sum(height)
+# sum the weights
+sum(weight)
+
+#test to see if we can practice commit
